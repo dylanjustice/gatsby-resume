e89ded3745374bacc110bf5c6d8a7c89cf335556
Dylan Justice <dylanjustice41@gmail.com>
2021-02-18 19:40:55 -0500

    Fix tslint errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,48 +8,62 @@ import ResumeLayout from "../components/templates/resume-layout";
 import { MarkdownRemark } from "../models/markdown-remark";
 
 const IndexPage = () => {
-	const data = useStaticQuery(graphql`
-		{
-			markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
-				frontmatter {
-					address
-					email
-					jobTitle
-					name
-					objectives
-					phone
-					skills
-					title
-					workExperience {
-						company
-						dates
-						location
-						responsibilities
-						title
-					}
-					education {
-						degree
-						location
-						school
-						year
-					}
-				}
-			}
-		}
-	`);
+  const data = useStaticQuery(graphql`
+    {
+      markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+        frontmatter {
+          address
+          email
+          jobTitle
+          name
+          objectives
+          phone
+          skills
+          title
+          workExperience {
+            company
+            dates
+            location
+            responsibilities
+            title
+          }
+          education {
+            degree
+            location
+            school
+            year
+          }
+        }
+      }
+    }
+  `);
 
-	const markdownRemark: MarkdownRemark = data.markdownRemark;
-	return (
-		<div className="g-application">
-			<Helmet>
-				<link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
-				<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
-				<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
-				<link rel="manifest" href="/assets/site.webmanifest" />
-			</Helmet>
-			<ResumeLayout frontmatter={markdownRemark.frontmatter} />
-		</div>
-	);
+  const markdownRemark: MarkdownRemark = data.markdownRemark;
+  return (
+    <div className="g-application">
+      <Helmet>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/assets/site.webmanifest" />
+      </Helmet>
+      <ResumeLayout frontmatter={markdownRemark.frontmatter} />
+    </div>
+  );
 };
 
 export default IndexPage;
