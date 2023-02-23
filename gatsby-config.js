572302d0af077f508bb658a7513dd394f3260d87
Dylan Justice <dylan.justice@lexisnexis.com>
2023-02-23 08:57:31 -0500

    Upgrade gatsby and dependencies to v5

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,24 @@
 module.exports = {
-  siteMetadata: {
-    name: `Dylan Justice Resume`,
-    tagline: `Gatsby + SASS + Typescript = 💪`,
-  },
-  plugins: [
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-typescript`,
-    `gatsby-plugin-tslint`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `pages`,
-        path: `${__dirname}/src/pages`,
-      },
+    siteMetadata: {
+        name: `Dylan Justice Resume`,
+        tagline: `Gatsby + SASS + Typescript = 💪`,
     },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        // CommonMark mode (default: true)
-        commonmark: true,
-        // Footnotes mode (default: true)
-        footnotes: true,
-        // Pedantic mode (default: true)
-        pedantic: true,
-        // GitHub Flavored Markdown mode (default: true)
-        gfm: true,
-        // Plugins configs
-        plugins: [],
-      },
-    },
-  ],
+    plugins: [
+        `gatsby-plugin-sass`,
+        `gatsby-plugin-typescript`,
+        `gatsby-plugin-tslint`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `pages`,
+                path: `${__dirname}/src/pages`,
+            },
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [],
+            },
+        },
+    ],
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,65 +7,73 @@ import ResumeLayout from "../components/templates/resume-layout";
 import { MarkdownRemark } from "../models/markdown-remark";
 
 const IndexPage = () => {
-  const data = useStaticQuery(graphql`
-    {
-      markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
-        frontmatter {
-          address
-          email
-          jobTitle
-          name
-          objectives
-          phone
-          skills
-          title
-          workExperience {
-            company
-            dates
-            location
-            responsibilities
-            title
-          }
-          education {
-            degree
-            location
-            school
-            year
-          }
+    const data = useStaticQuery(graphql`
+        {
+            markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+                frontmatter {
+                    address
+                    email
+                    jobTitle
+                    name
+                    objectives
+                    phone
+                    skills
+                    title
+                    workExperience {
+                        company
+                        dates
+                        location
+                        responsibilities
+                        title
+                    }
+                    education {
+                        degree
+                        location
+                        school
+                        year
+                    }
+                }
+            }
         }
-      }
-    }
-  `);
+    `);
 
-  const markdownRemark: MarkdownRemark = data.markdownRemark;
-  return (
-    <div className="g-application">
-      <Helmet>
+    const markdownRemark: MarkdownRemark = data.markdownRemark;
+    return (
+        <div className="g-application">
+            <ResumeLayout frontmatter={markdownRemark.frontmatter} />
+        </div>
+    );
+};
+
+export const Head = () => (
+    <React.Fragment>
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/images/apple-touch-icon.png"
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/images/apple-touch-icon.png"
         />
         <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/images/favicon-32x32.png"
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/images/favicon-32x32.png"
         />
         <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/images/favicon-16x16.png"
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/images/favicon-16x16.png"
         />
         <link rel="manifest" href="/assets/site.webmanifest" />
         <title>Dylan Justice</title>
-        <meta name="description" content="A simple way to display and manage my resume" />
-        <meta name="keywords" content="Dylan Justice, resume, software engineer, CV, professional" />
-      </Helmet>
-      <ResumeLayout frontmatter={markdownRemark.frontmatter} />
-    </div>
-  );
-};
-
+        <meta
+            name="description"
+            content="A simple way to display and manage my resume"
+        />
+        <meta
+            name="keywords"
+            content="Dylan Justice, resume, software engineer, CV, professional"
+        />
+    </React.Fragment>
+);
 export default IndexPage;
